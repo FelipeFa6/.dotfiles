c0862fc8ccfcca45d89115a1357c5d51d1529806
FelipeFa6 <felipe.farias.e1@gmail.com>
2024-09-15 17:32:48 -0300

    Added tmux to mklinks.sh

--- openbsd/mklinks.sh
+++ openbsd/mklinks.sh
@@ -6,6 +6,7 @@ mkdir -p $HOME/.config
 ln -s $(pwd)/.kshrc $HOME/
 ln -s $(pwd)/.profile $HOME/
 ln -s $(pwd)/.xinitrc $HOME/
+ln -s $(pwd)/.tmux.conf $HOME/
 ln -s $(pwd)/.config/* $HOME/.config/
 
 mkdir -p $HOME/.local/share/
