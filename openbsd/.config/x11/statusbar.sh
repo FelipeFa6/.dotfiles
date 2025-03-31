2835523b22672b605fb13227aa4b3b8f88fa10f6
FelipeFa6 <felipe.farias.e1@gmail.com>
2025-03-31 14:03:39 -0300

    Removed openbsd to be moved at another branch

--- openbsd/.config/x11/statusbar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-BATTERY=$(cat /sys/class/power_supply/BAT0/capacity)
-DATE=$(date)
-
-xsetroot -name " [ ${BATTERY} % ] [ ${DATE} ] "
--- openbsd/.kshrc
+++
@@ -1,7 +0,0 @@
-#!/bin/ksh
-
-set -o vi
-
-alias gs='git status'
-
-PS1="[\u@\w] $ "
