cdc5aba958388f6aae7f2bd2014f89db97724b44
FelipeFa6 <felipe.farias.e1@gmail.com>
2026-08-16 19:15:54 -0400

    Update

--- home/felipe/.config/x11/statusbar.sh
+++ home/felipe/.config/x11/statusbar.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-#date
-DATE=$(date)
+# Get date in DD/MM/YYYY 24hr format
+DATE=$(date +"%d/%m/%Y %H:%M")
 
-#battery
+# Battery status
 BATT_PATH="/sys/class/power_supply/BAT0"
 BATT_STATUS="$(cat $BATT_PATH/status)"
 BATT_CAPACITY="$(cat $BATT_PATH/capacity)"
 
+# Check internet connection
+check_internet() {
+        ping -c 1 -W 2 1.1.1.1 >/dev/null 2>&1
+}
 
-xsetroot -name " [$BATT_STATUS $BATT_CAPACITY %] [ ${DATE} ] "
+if check_internet ; then
+  CONNECTION="Online"
+else
+  CONNECTION="Offline"
+fi
+
+# Set the status bar
+xsetroot -name " [$BATT_STATUS $BATT_CAPACITY %] [ ${DATE} ] [ ${CONNECTION} ]"
+
