32fc0e94015b40c3a89881403e213307f10747e9
FelipeFa6 <felipe.farias.e1@gmail.com>
2026-08-17 01:17:16 -0400

    Added new binaries and fixed scripts

--- .config/x11/statusbar.sh
+++ .config/x11/statusbar.sh
@@ -1,24 +1,11 @@
 #!/bin/sh
+BIN="$HOME/bin"
 
-# Get date in DD/MM/YYYY 24hr format
-DATE=$(date +"%d/%m/%Y %H:%M")
+BAT=$("$BIN/get-battery" 2>/dev/null) || BAT='?'
+NET=$("$BIN/check-internet" 2>/dev/null) || NET='?'
+DATE=$(date +'%d/%m/%Y %H:%M')
 
-# Battery status
-BATT_PATH="/sys/class/power_supply/BAT0"
-BATT_STATUS="$(cat $BATT_PATH/status)"
-BATT_CAPACITY="$(cat $BATT_PATH/capacity)"
+[ -n "$BAT" ] || BAT='?'
+[ -n "$NET" ] || NET='?'
 
-# Check internet connection
-check_internet() {
-        ping -c 1 -W 2 1.1.1.1 >/dev/null 2>&1
-}
-
-if check_internet ; then
-  CONNECTION="Online"
-else
-  CONNECTION="Offline"
-fi
-
-# Set the status bar
-xsetroot -name " [$BATT_STATUS $BATT_CAPACITY %] [ ${DATE} ] [ ${CONNECTION} ]"
-
+xsetroot -name " [$BAT] [ $DATE ] [ $NET ]"
